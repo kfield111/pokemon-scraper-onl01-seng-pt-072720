13f3796fa7a530c8e2a09cf941c1cf545d342a5e
Kelin Field <kfield111@hotmail.com>
2020-10-12 23:59:00 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,8 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
-  def initialize (id = nil, name, type, db)
-    @id = id
-    @name = name
-    @type = type
-    @db = db
+  def initialize (pokemon)
+    @pokemon = pokemon
   end
 
 
