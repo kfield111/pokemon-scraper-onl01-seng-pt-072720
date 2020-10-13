7d1e8cb07158d57cdb38a43cab1422fdf8f87026
Kelin Field <kfield111@hotmail.com>
2020-10-13 01:14:06 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,11 +3,8 @@ require 'pry'
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
-  def initialize (id = nil, name, type, db)
-    @id = id
-    @name = name
-    @type = type
-    @db = db
+  def initialize (testy)
+    @testy = testy
   end
 
   def self.save (name, type, db)
