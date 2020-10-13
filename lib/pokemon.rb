61e166b66d87c39555303035c6bcfda53d6f2452
Kelin Field <kfield111@hotmail.com>
2020-10-13 00:42:14 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,7 @@ class Pokemon
   end
 
   def self.find(id, db)
-    pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
     name = pokemon[1]
     type = pokemon[2]
 
