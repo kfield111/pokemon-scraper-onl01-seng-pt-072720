de23e6abc5ee33da52e01ed44edd860669d16b57
Kelin Field <kfield111@hotmail.com>
2020-10-13 00:04:43 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -6,7 +6,16 @@ class Pokemon
   end
 
   def save
-
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO pokemon (name, type, db)
+        VALUES (?, ?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.type, self.db)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
   end
 
 
