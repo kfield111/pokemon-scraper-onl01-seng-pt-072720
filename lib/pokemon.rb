5d9b6996f5988f643b8ff11cfe45f958f17d11b3
Kelin Field <kfield111@hotmail.com>
2020-10-13 00:44:41 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,7 +20,7 @@ class Pokemon
     name = new_pokemon[1]
     type = new_pokemon[2]
 
-    pokemon = Pokemon.new(id: id, name: name, type: type, db: db)
+    pokemon = Pokemon.new(id, name, type, db)
   end
 
 
