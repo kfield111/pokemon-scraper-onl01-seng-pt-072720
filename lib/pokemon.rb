622ae9b0cc64a31105bead684ee2f4ba02738d51
Kelin Field <kfield111@hotmail.com>
2020-10-13 00:09:54 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -7,7 +7,7 @@ class Pokemon
 
   def save
     sql = <<-SQL
-      INSERT INTO pokemon (name, type, db)
+      INSERT INTO pokemon (name, type)
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.type)
